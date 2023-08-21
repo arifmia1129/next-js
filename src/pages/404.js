1e17458685c699de67a3f4729a695c5df2d11b6c
arifmia1129 <arifmia1129@gmail.com>
2023-08-21 20:01:37 +0600

    we can custom not found page in next js. just create file 404.js into pages folder in root

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/019/633/898/original/404-error-in-desktop-free-png.png"
+        alt="not found"
+        width="60%"
+      />
+    </div>
+  );
+};
+
+export default NotFound;
--- src/pages/products/[...slug].js
+++ src/pages/products/[...slug].js
@@ -1,7 +1,10 @@
+import { useRouter } from "next/router";
+
 export default function ProductDetailsWithFilter() {
+  const router = useRouter();
   return (
     <div>
-      <h1>This is product details page with filtering </h1>
+      <h1>This is product details page with filtering {router.query.slug}</h1>
     </div>
   );
 }
